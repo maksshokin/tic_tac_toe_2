48f258bbc30c6c7fed307c9bd751fd061971590a
Maksim Shokin <maksshokin90@gmail.com>
2024-03-01 08:49:04 +0300

    123

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 from gameparts import Board
 
 def main():
+    """Функция, выводящая данные."""
     game = Board()
     game.display()
     game.make_move(1, 1, 'X')
     print('Ход сделан!')
     game.display()
 
+print(print.__doc__)
 
 if __name__ == '__main__':
     main() 
--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,11 +1,15 @@
 class Board:
+    """Класс, который описывает игровое поле."""
+
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
     def make_move(self, row, col, player):
+        """Функция, делающая ход."""
         self.board[row][col] = player
 
     def display(self):
+        """Вывод дисплея."""
         for row in self.board:
             print('|'.join(row))
             print('-' * 5) 
