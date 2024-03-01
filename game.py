fc767f547f77660c103fea100580728a11c4716f
Maksim Shokin <maksshokin90@gmail.com>
2024-03-01 08:53:22 +0300

    123

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
 from gameparts import Board
 
 def main():
-    """Функция, выводящая данные."""
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+    # Тут пользователь вводит координаты ячейки.
+    row = int(input('Введите номер строки: '))
+    column = int(input('Введите номер столбца: '))
+    # В метод make_move передаются те координаты, которые ввёл пользователь.
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
-
-print(print.__doc__)
 
 if __name__ == '__main__':
     main() 
